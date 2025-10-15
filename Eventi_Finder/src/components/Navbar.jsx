64eb7c929d07c3266830559b2f0f3a0f4dc47370
Fabio <fadangelo@itsmeccanicabruzzo.eu>
2025-10-15 11:34:16 +0200

    2. Navbar inizio

---
+++ Eventi_Finder/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import logo from '../assets/logo.jpg'
+
+export default function Navbar(){
+    return(
+        <div>
+            <img src={logo} className="logo" />
+            <h1>Application: Event Finder 2.0</h1>
+        </div>
+    );
+}
